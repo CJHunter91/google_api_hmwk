03bc0477cab1cb89edd3dfb012cb8d1f5bcc137f
Chris Hunter <orkem2@gmail.com>
2017-09-05 16:18:33 +0100

    added to edinburgh button

--- public/app.js
+++ public/app.js
@@ -2,10 +2,14 @@
 var init = function(){
   var coords = {lat: 51.507351, lng: -0.122758}
   var operaHouse = {lat: 51.512922, lng: -0.122201}
-  var mapDiv = document.querySelector('#main-map');
-  var bounceButton = document.querySelector('#button-bounce-markers');
+
   var mainMap = new MapWrapper(mapDiv,coords, 10);
 
+  var mapDiv = document.querySelector('#main-map');
+  var bounceButton = document.querySelector('#button-bounce-markers');
+  var toEdinburghButton = document.querySelector('#to-edinburgh');
+
+  toEdinburghButton.addEventListener('click', mainMap.setCenter(edinburgh));
   bounceButton.addEventListener('click', mainMap.bounceMarker.bind(mainMap))
   mainMap.addMarker(coords);
   mainMap.addMarker(operaHouse);
